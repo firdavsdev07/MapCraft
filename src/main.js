0f6f7e6a0195aa47cff651a89abda8ed830ecf65
firdavsdev07 <firdavsnormurodov435@gmail.com>
2025-10-08 06:53:58 +0500

    The ability to draw lines has been added.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import mapboxgl from "mapbox-gl";
-import { createIcons, icons } from "lucide"; // 👈 icons ham import qilamiz
+import { createIcons, icons } from "lucide";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@ui/style.css";
 
-// 👇 Endi barcha Lucide ikonlarni yaratamiz
 createIcons({ icons });
 
 const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_API_TOKEN;
@@ -11,12 +10,14 @@ const markerBtn = document.getElementById("marker");
 const helpBtn = document.getElementById("help");
 const myLocationBtn = document.getElementById("my-location");
 const coordinatesView = document.getElementById("coordinates");
+const lineBtn = document.getElementById("line");
 
 const result = document.createElement("p");
 result.className = "px-1.5 py-1 rounded-sm mt-1";
 coordinatesView.appendChild(result);
 
 let markerArr = [];
+let lineCoordinates = [];
 let mode = "none";
 let activeButton = null;
 
@@ -39,11 +40,35 @@ const map = new mapboxgl.Map({
 });
 
 map.on("load", () => {
+  map.addSource("lineLayerId", {
+    type: "geojson",
+    data: {
+      type: "Feature",
+      geometry: {
+        type: "LineString",
+        coordinates: [],
+      },
+    },
+  });
+
+  map.addLayer({
+    id: "lineLayerId",
+    source: "lineLayerId",
+    type: "line",
+    layout: {
+      "line-join": "round",
+      "line-cap": "round",
+    },
+    paint: {
+      "line-color": "#00ffea",
+      "line-width": 3,
+    },
+  });
   markerBtn.onclick = () => activate(markerBtn, "marker");
   map.on("click", (e) => {
+    const { lng, lat } = e.lngLat;
     // marker yaratish
     if (mode === "marker") {
-      const { lng, lat } = e.lngLat;
       const marker = new mapboxgl.Marker({
         color: "#fa1f0f",
         draggable: true,
@@ -57,6 +82,18 @@ map.on("load", () => {
     }
 
     //line chizish uchun
+    lineBtn.onclick = () => activate(lineBtn, "line");
+    if (mode === "line") {
+      lineCoordinates.push([lng, lat]);
+      map.getSource("lineLayerId").setData({
+        type: "Feature",
+        geometry: {
+          type: "LineString",
+          coordinates: lineCoordinates,
+        },
+      });
+      result.textContent = `[ ${lng.toFixed(6)}, ${lat.toFixed(6)} ]`;
+    }
   });
   // o'z joyini aniqlash
   myLocationBtn.onclick = () => {
@@ -74,26 +111,22 @@ map.on("load", () => {
         },
       };
 
-      if (map.getSource("myLocationData")) {
-        map.getSource("myLocationData").setData(myLocationData);
-      } else {
-        map.addSource("myLocationData", {
-          type: "geojson",
-          data: myLocationData,
-        });
+      map.addSource("myLocationData", {
+        type: "geojson",
+        data: myLocationData,
+      });
 
-        map.addLayer({
-          id: "myLocationData",
-          type: "circle",
-          source: "myLocationData",
-          paint: {
-            "circle-radius": 6,
-            "circle-stroke-width": 2,
-            "circle-color": "#fa1f0f",
-            "circle-stroke-color": "#fff",
-          },
-        });
-      }
+      map.addLayer({
+        id: "myLocationData",
+        type: "circle",
+        source: "myLocationData",
+        paint: {
+          "circle-radius": 6,
+          "circle-stroke-width": 2,
+          "circle-color": "#fa1f0f",
+          "circle-stroke-color": "#fff",
+        },
+      });
 
       // faqat mavjud <p> matnini yangilaymiz
       result.textContent = `[ ${longitude.toFixed(6)}, ${latitude.toFixed(6)} ]`;
